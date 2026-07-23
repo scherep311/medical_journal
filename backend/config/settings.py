c87cc1fc354a91aaf7fbc63c614d4f6cbfd5f375
scherep <scherep@MacBook-Air-scherep.local>
2026-07-23 12:54:11 +0500

    access/refresh

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -153,7 +153,8 @@ REST_FRAMEWORK = {
 
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=8),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
